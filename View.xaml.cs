26a3c3e08a487a1c3e147269e2945291808410a3
vitt1984 <vittorio.turri@gmail.com>
2014-11-25 22:58:02 +0100

    Imported project

---
+++ View.xaml.cs
@@ -0,0 +1,24 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Vittorio
+ * Date: 18/11/2014
+ * Time: 20:23
+ * 
+ */
+using System;
+using System.Windows;
+
+namespace Test2
+{
+	/// <summary>
+	/// Interaction logic for View.xaml
+	/// </summary>
+	public partial class View : Window
+	{
+		public View()
+		{
+			InitializeComponent();
+		}
+	}
+	
+}
---
+++ Window1.xaml.cs
@@ -0,0 +1,31 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: TOSHIBA
+ * Date: 18/11/2014
+ * Time: 22:33
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+
+namespace Test2
+{
+	/// <summary>
+	/// Interaction logic for Window1.xaml
+	/// </summary>
+	public partial class Window1 : Window
+	{
+		public Window1()
+		{
+			InitializeComponent();
+		}
+	}
+}
